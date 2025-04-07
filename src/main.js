87cbc66feb3c09fd4595f80fca3a9af5236ef0e9
Özge SEVİM <ozgsvm@gmail.com>
2025-04-07 13:43:06 -0700

    file paths fixed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-document.createElement("div");
+import { getMovies } from "./js/weeklyTrends";
+import { catalogFun } from "./js/catalog";
+import { hero } from "./js/hero";
+import { upComing } from "./js/upComing";
+import { header } from "./js/header";
 
-document.createElement("a");
-
-document.createElement("img");
+hero();
+getMovies();
+catalogFun();
+upComing();
+header();
